5df1130eca15c2c63de69daf5e9acdd22686f118
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-15 10:30:41 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,32 @@
+
+var num=prompt("Enter the number");
+
+if(num>0)
+
+
+
+    console.log("Positive");
+
+
+     else if(num<0)
+
+
+
+    console.log("Negative");
+
+
+    else (num==0)
+
+
+
+    console.log("Zero");
+
+
+
+
+
+
+
+
+
+
